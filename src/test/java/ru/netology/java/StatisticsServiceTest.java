40141a7e57013c078d7e78d70d3b329a16fa9ed4
Ilya Kiryushin <ilya.cyr@gmail.com>
2025-09-04 00:20:18 +0300

    Test commit

--- src/test/java/ru/netology/java/StatisticsServiceTest.java
+++ src/test/java/ru/netology/java/StatisticsServiceTest.java
@@ -11,9 +11,20 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIfOne() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
